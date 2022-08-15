620e3feb3462206f8db6cc2c103854e5c323f12d
Chambers <bobhus394@gmail.com>
2022-08-15 16:52:10 +0800

    調整nav事件架構

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,42 @@
 const menu = document.querySelector('.menu')
 const nav = document.querySelector('.nav')
+const navLinks = document.querySelectorAll('.nav-link')
+const sectionList = document.querySelectorAll('section')
+
+const scrollHandler = () => {
+  let current = ''
+  const pageOffset = window.pageYOffset
+  sectionList.forEach(sect => {
+    console.log('pageOffset:', pageOffset)
+    console.log(sect.dataset.name, 'offsetTop:', sect.offsetTop)
+    if (pageOffset >= sect.offsetTop - 105) {
+      current = sect.dataset.name
+    }
+  })
+  navLinks.forEach(link => {
+    if (link.classList.contains('active')) {
+      link.classList.remove('active')
+    }
+  })
+  navLinks.forEach(link => {
+    if (link.dataset.name === current) {
+      link.classList.add('active')
+    }
+  })
+}
 
 const toggleMenu = () => {
   nav.classList.toggle('active')
 }
 
+const closeMenu = () => {
+  nav.classList.remove('active')
+}
+
 
 
 menu.addEventListener('click', toggleMenu)
+navLinks.forEach(link => {
+  link.addEventListener('click', closeMenu)
+})
+window.addEventListener('scroll', scrollHandler)
